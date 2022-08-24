ca534f577bd64448234da6cf7a542d91a664569d
Isadora Coelho <isadora.araujo1@unioeste.br>
2022-08-24 16:39:36 -0300

    nada

---
+++ tarefa-dia-2/pratica1.js
@@ -0,0 +1,11 @@
+//seu nome
+//seu email
+//sua profissão
+//sua idade
+//o lugar que você sonha um dia conhecer (pode ser um país, cidade ou um local mesmo)
+
+let nome = "Isadora Coelho"
+let email = "izacoelho33@gmail.com"
+let profissao = "Estudante"
+let idade = 17
+let local = "Russia"
