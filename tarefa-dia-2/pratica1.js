bad46255eed01b3662bd59fa83659022ea144acc
Isadora Coelho <isadora.araujo1@unioeste.br>
2022-08-24 16:56:40 -0300

    final

--- tarefa-dia-2/pratica1.js
+++ tarefa-dia-2/pratica1.js
@@ -9,3 +9,8 @@ let email = "izacoelho33@gmail.com"
 let profissao = "Estudante"
 let idade = 17
 let local = "Russia"
+let mensagem 
+
+mensagem  = "Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para "+ local + ". Confirma pra gente alguns dados? A sua idade é "+ idade + ", você é " + profissao + " e o seu email é "+ email +"? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+console.log(mensagem)
