8eb2cb070f4b02220b6376af9fb058915a54a22b
isadora.araujo1@unioeste.br <isadora.araujo1@unioeste.br>
2022-08-25 13:56:13 -0300

    t3

---
+++ tarefa-dia-3/pratica2.js
@@ -0,0 +1,10 @@
+let n = 9
+let cont = 1
+
+console.log("A tabuada de " + n + " é:")
+while (cont <= 10)
+{
+    mult = cont*n
+    console.log(n + "x" + cont + " = " + mult)
+    cont++
+}
---
+++ tarefa-dia-3/pratica3.js
@@ -0,0 +1,9 @@
+let n = 16
+let cont = 1
+
+while(cont <= n)
+{
+    if(cont%2)
+    console.log(cont)
+    cont++
+}
