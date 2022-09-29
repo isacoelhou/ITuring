b86360924b2af18954b55fde3d5a135e6144cd93
isadora.araujo1@unioeste.br <isadora.araujo1@unioeste.br>
2022-09-29 10:12:40 -0300

    html

---
+++ tarefa-dia-4/sc.js
@@ -0,0 +1,7 @@
+const inputt = document.getElementById("inpp")
+
+function addtar(){
+    const item = document.createElement("li")
+    const item = document.querySelector("#lista")
+    lista.appendChild(item)
+}
